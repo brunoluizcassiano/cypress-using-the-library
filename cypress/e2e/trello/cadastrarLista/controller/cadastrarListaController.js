171242043e1798783edc95851df29bb64838da5c
Bruno Cassiano <bruno15l@hotmail.com>
2026-05-03 20:05:19 -0300

    teste6

--- cypress/e2e/trello/cadastrarLista/controller/cadastrarListaController.js
+++ cypress/e2e/trello/cadastrarLista/controller/cadastrarListaController.js
@@ -5,37 +5,26 @@ class controllerCadastrarLista{
         const key = "e385e6347369be467a41dcec9eb37945";
         const token = "daf4095434e4fd02ad302c145877c6dfa5305ac1f3c68f8a30e7627333a0b87f";
 
-        // const uri = "https://api.trello.com/";
-        // const path = `1/lists?name=TO DO&idboard=${idboard}&key=${key}&token=${token}`;
-        // const fileData = "cypress/data/createList.json";
-
-        // cy.requestRestFullFileData("cadastroList", "POST", uri, path, fileData);
-
         const uri = "https://api.trello.com";
         const path = `1/lists`;
-        const fileData = "cypress/data/createList.json";
         let queryParams = { 
             'name': 'TO DO',
-            'idBoard': idboard
+            'idBoard': idboard,
+            'key': key,
+            'token': token
         };
-        let authNames = ['APIKey', 'APIToken'];
-        cy.readFile('resources/authentications.json').then(resp => {
-            cy.requestRestFul({
-                requestAlias: "postLists",
-                uri: uri,
-                path: path,
-                httpMethod: 'POST',
-                queryParams: queryParams,
-                authNames: authNames,
-                authenticationsJson: resp
-            });
-        })
+        cy.request({
+            method: 'POST',
+            url: `${uri}/${path}`,
+            qs: queryParams,
+            failOnStatusCode: false
+        }).as('postLists');
         
     }
 
     validarStatusCode(){
-        cy.getResponseStatusCode().then(response => {
-            expect(response).to.eq(200)
+        cy.get('@postLists').then(response => {
+            expect(response.status).to.eq(200)
         })
     }
 
--- cypress/e2e/trello/cadastrarLista/features/listaTrello.feature
+++ cypress/e2e/trello/cadastrarLista/features/listaTrello.feature
@@ -1,6 +1,6 @@
 Feature: Trello
 
-    @coe @idCenarioZephyr1 @idHistoria @funcionalidade @rest @regressivo @massa @identificacao
+    @contratos @coe @idCenarioZephyr1 @idHistoria @funcionalidade @rest @regressivo @massa @identificacao
     Scenario: Cadastrar uma nova lista no meu board
         Given que estamos no fluxo de cadastro de lista no meu board
         When realizo o cadastramento de uma nova lista no board
